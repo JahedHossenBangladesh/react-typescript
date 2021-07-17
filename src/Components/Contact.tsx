a1ecd7872666615d006a279a5889564e6885303b
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-07-18 01:41:41 +0600

    The concept is clear

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -2,12 +2,14 @@ import React from "react";
 interface Iprops {
   name: string;
   email?: string;
+  handleRemove: (email: string) => void;
 }
-function Contact({ name, email = "N/A" }: Iprops) {
+function Contact({ name, email = "N/A", handleRemove }: Iprops) {
   return (
     <div>
       <h1>Name :{name}</h1>
       <h2>{email}</h2>
+      <button onClick={() => handleRemove(email)}>Remove</button>
     </div>
   );
 }
--- src/Components/Contacts.tsx
+++ src/Components/Contacts.tsx
@@ -19,19 +19,40 @@ export default function Contacts() {
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setContacts({ ...contacts, [e.target.name]: e.target.value });
   };
-  //   const handleRemove =
+  const handleRemove = (email: string) => {
+    const newSetContact = contactList.filter(
+      (contact) => contact.email !== email
+    );
+    setContactsList(newSetContact);
+  };
   return (
     <div>
       <h1>Contact list</h1>
       <div>
-        <input type="text" value={contacts.name} onChange={onChange} />
-        <input type="text" value={contacts.email} onChange={onChange} />
+        <input
+          type="text"
+          value={contacts.name}
+          name="name"
+          placeholder="Enter Your Name"
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          value={contacts.email}
+          name="email"
+          placeholder="Enter your Email"
+          onChange={onChange}
+        />
         <button onClick={onClick}>Add</button>
-        {contactList.map((contact) => (
-          <Contact key={contact.name} name={contact.name} />
-        ))}
       </div>
-      \//.,
+      {contactList.map((c) => (
+        <Contact
+          key={c.name}
+          name={c.name}
+          email={c.email}
+          handleRemove={handleRemove}
+        />
+      ))}
     </div>
   );
 }
